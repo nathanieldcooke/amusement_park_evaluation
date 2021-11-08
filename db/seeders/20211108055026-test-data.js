d6e4fd69074da54332f5845c7f6516119fb32514
nathanieldcooke <nathanield.cooke@gmail.com>
2021-11-07 22:38:20 -0800

    add parks model migration and put template for parks - Phase 7

---
+++ db/seeders/20211108055026-test-data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Parks', [
+      {
+        parkName: 'Nathan Dino Park',
+        city: 'Las Vegas',
+        provinceState: 'Nevada',
+        country: 'United States',
+        opened: new Date(),
+        size: 'Big',
+        description: 'A randome park for seed data',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Parks', null, {});
+  }
+};
--- routes.js
+++ routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const { env } = require('./config')
+const db = require('./db/models')
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
+
+router.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
 })
+
+router.get('/parks', asyncHandler(async (req, res) => {
+    console.log("DBz: ",db)
+    const parks = await db.Park.findAll({
+        order: [
+            ['parkName', 'ASC']
+        ]
+    })
+    res.render('park-list', {title: 'Parks', parks})
+}))
 
 if (env !== "production") {
     router.get("/error-test", () => {
